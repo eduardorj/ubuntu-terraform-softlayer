0244c1ab3fdcc7bb590bfb7525cc59e3c1fd25fb
Eduardo Francellino <eduardo@br.ibm.com>
2019-03-26 13:49:47 -0300

    automation removed

--- ubuntu.tf
+++ ubuntu.tf
@@ -84,24 +84,11 @@ resource "ibm_compute_vm_instance" "softlayer_virtual_guest" {
 # Change root password
 echo "root:${var.password}" | chpasswd
 
-# Installing docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-
 # Fixing DNS issue
 rm -f /etc/resolv.conf
 ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 sed -i 's/ - update_etc_hosts/# - update_etc_hosts/' /etc/cloud/cloud.cfg
-
-sed -i 's/127.0.1.1	mycluster/${ibm_compute_vm_instance.softlayer_virtual_guest.ipv4_address}	mycluster/' /etc/hosts
-
-apt install git
-
-cd / && git clone https://github.com/eduardorj/jenkins-helm-edu.git
-
-mkdir /data/
-mkdir /data/jenkins
 
 EOF
 
